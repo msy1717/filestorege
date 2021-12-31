65ca3afd11490d4401a37153305128d7dfcb8d71
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-12-31 13:21:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
 from database.sql import add_user, query_msg, full_userbase
-chat_id = -1001706792857
+chat_id = -1001262520366
 
 
 #=====================================================================================##
